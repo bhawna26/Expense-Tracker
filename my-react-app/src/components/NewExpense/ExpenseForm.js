f148fb851e0b376e35cba917f94f89b26d0598c2
Bhawna Bhawna (Nokia) <bhawna.bhawna@nokia.com>
2023-06-09 14:00:51 +0530

    better way of managing multiple states

--- my-react-app/src/components/NewExpense/ExpenseForm.js
+++ my-react-app/src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './ExpenseForm.css';
 const ExpenseForm=()=>{
   // const[enteredTitle,setEnteredTitle]=useState('');
@@ -11,19 +11,19 @@ const ExpenseForm=()=>{
 });
 
   const titleChangeHandler=(event)=>{
-  setUserInput({
-    ...userInput,
-    enteredTitle:event.target.value});
+  setUserInput((prevState)=>{
+   return {...prevState,enteredTitle:event.target.value};
+  });
   };
   const dateChangeHandler=(event)=>{
-    setUserInput({
-      ...userInput,
-      enteredDate:event.target.value});
+    setUserInput((prevState)=>{
+      return {...prevState,enteredDate:event.target.value};
+     });   
   };
   const amountChangeHandler=(event)=>{
-    setUserInput({
-      ...userInput,
-      enteredAmount:event.target.value});
+    setUserInput((prevState)=>{
+      return {...prevState,enteredAmount:event.target.value};
+     });   
   };
   
 
